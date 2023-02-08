8723c1f690e9bf30d3a67c8a086e94035ccf19f5
wu-mn <wu_manning@retail-ai.jp>
2023-02-08 14:38:56 +0900

    number puzzle

--- src/App.js
+++ src/App.js
@@ -38,87 +38,87 @@ function App() {
     };
   }, []);
 
-  let r;
-  let colors = ["yellow", "pink", "green", "blue", "white", "gray"];
+  // let r;
+  // let colors = ["yellow", "pink", "green", "blue", "white", "gray"];
 
-  const engine = new Engine(0, 0, 600, 800, 0, 9.8);
-  r = new RectangleEntity(500, 50, 50, 400);
-  r.color = "green";
-  engine.entities.push(r);
+  // const engine = new Engine(0, 0, 600, 800, 0, 9.8);
+  // r = new RectangleEntity(500, 50, 50, 400);
+  // r.color = "green";
+  // engine.entities.push(r);
 
-  r = new RectangleEntity(0, 50, 50, 400);
-  r.color = "yellow";
-  engine.entities.push(r);
+  // r = new RectangleEntity(0, 50, 50, 400);
+  // r.color = "yellow";
+  // engine.entities.push(r);
 
-  r = new LineEntity(50, 300, 400, 350);
-  r.color = "orange";
-  engine.entities.push(r);
+  // r = new LineEntity(50, 300, 400, 350);
+  // r.color = "orange";
+  // engine.entities.push(r);
 
-  r = new LineEntity(500, 400, 100, 450);
-  r.color = "orange";
-  engine.entities.push(r);
+  // r = new LineEntity(500, 400, 100, 450);
+  // r.color = "orange";
+  // engine.entities.push(r);
 
-  for (let i = 0; i < 7; i++) {
-    for (let j = 0; j < 3; j++) {
-      r = new CircleEntity(i * 60 + 100, j * 60 + 100, 5, BodyStatic);
-      r.color = colors[j];
-      engine.entities.push(r);
-    }
-  }
+  // for (let i = 0; i < 7; i++) {
+  //   for (let j = 0; j < 3; j++) {
+  //     r = new CircleEntity(i * 60 + 100, j * 60 + 100, 5, BodyStatic);
+  //     r.color = colors[j];
+  //     engine.entities.push(r);
+  //   }
+  // }
 
-  const rand = (v) => {
-    return Math.floor(Math.random() * v);
-  };
+  // const rand = (v) => {
+  //   return Math.floor(Math.random() * v);
+  // };
 
-  for (let i = 0; i < 20; i++) {
-    r = new CircleEntity(rand(400) + 50, rand(200), 10, BodyDynamic);
-    r.color = colors[rand(5)];
-    r.velocity.x = rand(10) - 5;
-    r.velocity.y = rand(10) - 5;
-    engine.entities.push(r);
-  }
+  // for (let i = 0; i < 20; i++) {
+  //   r = new CircleEntity(rand(400) + 50, rand(200), 10, BodyDynamic);
+  //   r.color = colors[rand(5)];
+  //   r.velocity.x = rand(10) - 5;
+  //   r.velocity.y = rand(10) - 5;
+  //   engine.entities.push(r);
+  // }
 
-  const canvasRef = useRef(null);
+  // const canvasRef = useRef(null);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    const repaint = () => {
-      ctx.fillStyle = "black";
-      ctx.fillRect(0, 0, 600, 600);
-      engine.entities.forEach((e) => {
-        ctx.fillStyle = e.color;
-        ctx.strokeStyle = e.color;
-        switch (e.shape) {
-          case ShapeRectangle:
-            ctx.fillRect(e.x, e.y, e.w, e.h);
-            break;
-          case ShapeCircle:
-            ctx.beginPath();
-            ctx.arc(e.x, e.y, e.radius, 0, Math.PI * 2);
-            ctx.closePath();
-            ctx.fill();
-            break;
-          case ShapeLine:
-            ctx.beginPath();
-            ctx.moveTo(e.x0, e.y0);
-            ctx.lineTo(e.x1, e.y1);
-            ctx.stroke();
-            break;
-        }
-      });
-    };
+  // useEffect(() => {
+  //   const canvas = canvasRef.current;
+  //   const ctx = canvas.getContext("2d");
+  //   const repaint = () => {
+  //     ctx.fillStyle = "black";
+  //     ctx.fillRect(0, 0, 600, 600);
+  //     engine.entities.forEach((e) => {
+  //       ctx.fillStyle = e.color;
+  //       ctx.strokeStyle = e.color;
+  //       switch (e.shape) {
+  //         case ShapeRectangle:
+  //           ctx.fillRect(e.x, e.y, e.w, e.h);
+  //           break;
+  //         case ShapeCircle:
+  //           ctx.beginPath();
+  //           ctx.arc(e.x, e.y, e.radius, 0, Math.PI * 2);
+  //           ctx.closePath();
+  //           ctx.fill();
+  //           break;
+  //         case ShapeLine:
+  //           ctx.beginPath();
+  //           ctx.moveTo(e.x0, e.y0);
+  //           ctx.lineTo(e.x1, e.y1);
+  //           ctx.stroke();
+  //           break;
+  //       }
+  //     });
+  //   };
 
-    const tick = () => {
-      engine.step(0.01);
-      repaint();
-    };
-    setInterval(tick, 50);
-  }, []);
+  //   const tick = () => {
+  //     engine.step(0.01);
+  //     repaint();
+  //   };
+  //   setInterval(tick, 50);
+  // }, []);
 
   return (
     <>
-      <canvas ref={canvasRef} width="600" height="600"></canvas>
+      {/* <canvas ref={canvasRef} width="600" height="600"></canvas> */}
       <table id="table"></table>
     </>
   );
